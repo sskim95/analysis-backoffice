28e8dbada4abb2733e1862f3fa7ff0ab382da3f4
sungsookim <sskim950105@gmail.com>
2022-03-22 01:17:01 +0900

    Add vuetify layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,68 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-app-bar color="grey-lighten-2">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title style="cursor: pointer" @click="$router.push('/')">
+        보답
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer color="grey-darken-2" permanent v-model="drawer">
+      <v-list-item to="/">
+        Home
+      </v-list-item>
+      <v-list-item to="/about">
+        About
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      drawer: null,
+      menus: [
+        {
+          id: 0,
+          title: '메인메뉴 0',
+          route: '/main0',
+          subMenus: []
+        },
+        {
+          id: 1,
+          title: '메인메뉴 1',
+          route: '/main1',
+          subMenus: [
+            {
+              id: 11,
+              title: '서브메뉴 11',
+              route: '/sub11'
+            }
+          ]
+        },
+        {
+          id: 2,
+          title: '메인메뉴 2',
+          route: '/main2',
+          subMenus: [
+            {
+              id: 21,
+              title: '서브메뉴 21',
+              route: '/sub21'
+            },
+            {
+              id: 22,
+              title: '서브메뉴 22',
+              route: '/sub22'
+            }
+          ]
+        }
+      ],
+    }
+  }
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
